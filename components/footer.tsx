d453e0d57989331be518563e306bc9882b848629
khomma0312 <kijikuroneko@gmail.com>
2025-06-28 23:38:22 +0900

    fix: フッターのソーシャルアイコンを削除

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export const Footer = () => (
-  <footer className="bg-stone-50">
-    <div className="container mx-auto px-5 py-10">
+  <footer className="bg-white">
+    <div className="container mx-auto px-5 py-12">
       <div className="flex flex-col items-center gap-6">
         <div className="flex w-full flex-wrap justify-center gap-6 text-center">
           <div className="w-full sm:w-auto">
@@ -21,32 +20,6 @@ export const Footer = () => (
               サービス規約
             </Link>
           </div>
-        </div>
-        <div className="flex justify-center gap-4">
-          <Link href="#">
-            <Image
-              src="/about/social-1.svg"
-              alt="Social Icon 1"
-              width={24}
-              height={24}
-            />
-          </Link>
-          <Link href="#">
-            <Image
-              src="/about/social-2.svg"
-              alt="Social Icon 2"
-              width={24}
-              height={24}
-            />
-          </Link>
-          <Link href="#">
-            <Image
-              src="/about/social-3.svg"
-              alt="Social Icon 3"
-              width={24}
-              height={24}
-            />
-          </Link>
         </div>
         <p className="text-base text-primary-800">
           © 2025 Pantri. All rights reserved.
