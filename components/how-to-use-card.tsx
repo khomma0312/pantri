d87bdedf1a8ba36003dbb580dc473d0db5eb70df
khomma0312 <kijikuroneko@gmail.com>
2025-06-28 17:24:14 +0900

    add: 使い方のステップをわかりやすく示すようにレイアウトを調整

--- components/how-to-use-card.tsx
+++ components/how-to-use-card.tsx
@@ -4,14 +4,19 @@ interface HowToUseCardProps {
   image: string;
   title: string;
   description: string;
+  step: number;
 }
 
 export const HowToUseCard = ({
   image,
   title,
   description,
+  step,
 }: HowToUseCardProps) => (
-  <div className="flex flex-col gap-3">
+  <div className="grid grid-rows-subgrid row-span-3 gap-3 relative w-4/5 mx-auto md:w-full">
+    <div className="absolute -top-2 -left-2 z-10 bg-primary-600 text-white text-sm font-bold rounded-full w-8 h-8 flex items-center justify-center">
+      {step}
+    </div>
     <Image
       src={image}
       alt={title}
